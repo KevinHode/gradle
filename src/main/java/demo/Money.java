b3c691d5822797d4403855cd5becc8e5b6e4ba6d
Kevin Hode <kevin.hode.keeb@gmail.com>
2022-09-15 21:32:32 +0200

    Set the test with JUnit and is valid at 100 per cent

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.util.Objects;
+
 public class Money {
     private int amount;
     private String currency;
@@ -14,5 +16,16 @@ public class Money {
                 "amount=" + amount +
                 ", currency='" + currency + '\'' +
                 '}';
+    }
+
+    public Money add(Money that) {
+        return new Money(this.amount+that.amount,this.currency);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
     }
 }
